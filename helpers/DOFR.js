9c931b5c98bbbd7e7ab8b7bf43b9394af041f90b
Francois van der Ven <francoisvdven@gmail.com>
2015-10-02 15:50:12 +0200

    Progress on ForkVM

--- helpers/DOFR.js
+++ helpers/DOFR.js
@@ -1,10 +1,19 @@
 var debug = require("debug")("DOFR:DOFR"),
-	Obj = require('./Obj');
+	Obj = require('./Obj'),
+	ForkVM = require("./VirtualMachines/ForkVM");
 
 var DOFR = Obj.extend({
 	init: function(){
-	    debug("YEAHH");
-	    console.log("FUCK YEAH");
+		var processType = process.env.DOFR_PROCESS_TYPE;
+
+		if(processType === "worker")
+			debug("I am a worker!");
+		else {
+			debug("I am a master");
+
+			var vm = new ForkVM();
+			vm.start();
+		}
 	}
 });
 
--- helpers/VirtualMachines/ForkVM.js
+++ helpers/VirtualMachines/ForkVM.js
@@ -1,5 +1,5 @@
 var Obj = require('../Obj'),
-	VirtualMachine = require("VirtualMachine");
+	VirtualMachine = require("./VirtualMachine");
 
 /**
  * Mock VirtualMachine implementation for testing
@@ -7,8 +7,26 @@ var Obj = require('../Obj'),
 var ForkVM = Obj.extend(VirtualMachine, {
 	start: function() {
 	    var script = process.argv[1];
-	    var args = ["worker"];
-	    var childProcess = require('child_process').fork(script, args);
+	    var args = [];
+
+		function extend(target) {
+			var sources = [].slice.call(arguments, 1);
+			sources.forEach(function (source) {
+				for (var prop in source) {
+					target[prop] = source[prop];
+				}
+			});
+			return target;
+		}
+
+		var options = {
+			env: extend({
+				PORT: process.env.PORT ? process.env.PORT + 1 : 3001,
+				DOFR_PROCESS_TYPE: "worker"
+			}, process.env),
+			execArgv: ["--debug-brk=" + (process.debugPort + 1).toString()]
+		}
+	    var childProcess = require('child_process').fork(script, args, options);
 	}
 });
 
