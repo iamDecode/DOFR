bd31c4334ac59cd90753d1b62aecf6f0f564df1e
Dennis <d.collaris@me.com>
2015-10-03 17:21:17 +0200

    Remove redundant express stuff

--- app.js
+++ app.js
@@ -1,60 +1,25 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var debug = require("debug")("DOFR:DOFR"),
+  Obj = require("./lib/helpers/Obj"),
+  Worker = require("./lib/instances/Worker"),
+  Master = require("./lib/instances/Master");
 
-var app = express();
+/**
+ * Main program. Becomes a worker or master based off environment.
+ * 
+ * @class DOFR
+ * @constructor
+ */
+var DOFR = Obj.extend({
+  init: function() {
+    var processType = process.env.DOFR_PROCESS_TYPE;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    if (processType === "worker")
+      new Worker();
+    else
+      new Master();
+  }
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
+new DOFR();
